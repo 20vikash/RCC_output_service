9eb959331947e5b370d6df8b0f6756fefca2eaa2
ssvikash_20 <arisivikash@gmail.com>
2025-03-27 20:01:28 +0530

    grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	output "outputservice/grpc/server"
 
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	output.UnimplementedOutputServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":6971")
+	if err != nil {
+		log.Println(err)
+	}
+
+	s := grpc.NewServer()
+	output.RegisterOutputServiceServer(s, &Server{})
+
+	log.Printf("gRPC server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
