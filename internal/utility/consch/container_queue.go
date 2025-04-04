c394db79cba35f596ecf4e94e5d0a228b7a2ecb4
ssvikash_20 <arisivikash@gmail.com>
2025-04-04 13:49:28 +0530

    Stub for ConQueue

--- internal/utility/consch/container_queue.go
+++ internal/utility/consch/container_queue.go
@@ -1,4 +1,25 @@
 package consch
 
 type ConQueue struct {
+	Language string
+	Code     string
+	Jid      string
+
+	Next *ConQueue
+}
+
+func (q *ConQueue) AddCode() {
+
+}
+
+func (q *ConQueue) LatestCode() {
+
+}
+
+func (q *ConQueue) AckCode() {
+
+}
+
+func (q *ConQueue) PeekCode() {
+
 }
