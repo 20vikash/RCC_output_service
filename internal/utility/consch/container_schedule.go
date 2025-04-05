8a46d7df74e9743b9c244c4f057c32cfed7b7cee
ssvikash_20 <arisivikash@gmail.com>
2025-04-05 20:01:06 +0530

    STUB for container scheduling

--- internal/utility/consch/container_schedule.go
+++ internal/utility/consch/container_schedule.go
@@ -1,5 +1,37 @@
 package consch
 
-var noOfRunners = 5
+import "fmt"
 
-var runners = make(map[bool]string)
+var noOfRunners = 5
+var noOfLanguages = 1
+
+var languages = []string{"python"}
+var runners = make(map[string]bool)
+
+var pqChan = make(chan bool)
+var pcChan = make(chan bool)
+
+var pythonQueue = ConQueue{}
+
+func init() {
+	for range noOfLanguages {
+		for j := range noOfRunners {
+			runners[fmt.Sprintf("rcc-%s_runner-%s", languages[j], string(rune(j)))] = true
+		}
+	}
+
+	go PythonSchedule()
+}
+
+func AddToPythonQueue(language, code, jid string) {
+	pythonQueue.AddCode(language, code, jid)
+	pqChan <- true
+}
+
+func DoneExec() {
+	pcChan <- true
+}
+
+func PythonSchedule() {
+
+}
