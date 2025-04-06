c13e10c48e53ad9e1b87922fe1a808f9c526017f
ssvikash_20 <arisivikash@gmail.com>
2025-04-06 09:08:23 +0530

    Wrap setting pqChan in a goroutine to not get blocked

--- internal/utility/consch/container_schedule.go
+++ internal/utility/consch/container_schedule.go
@@ -28,7 +28,10 @@ func init() {
 
 func AddToPythonQueue(language, code, jid string) {
 	pythonQueue.AddCode(language, code, jid)
-	pqChan <- true
+
+	go func() {
+		pqChan <- true
+	}()
 }
 
 func PyDoneExec(number int) {
